a49365458428b0863b1658cf628ee7c2428f69bc
Enrico Benedetti <enrico_benedetti@yahoo.com>
2016-09-03 17:08:41 +0200

    LedDisplay

---
+++ src/main/scala/tutorial/webapp/LedDisplay.scala
@@ -0,0 +1,46 @@
+package tutorial.webapp
+
+import japgolly.scalajs.react.ReactElement
+import japgolly.scalajs.react.vdom.prefix_<^._
+
+import scala.collection.mutable
+import scala.scalajs.js.annotation.JSExport
+
+
+/**
+  * Created by enrico on 9/3/16.
+  */
+class LedDisplay(cellSize : Int, margin : Int, width : Int, height : Int) {
+  val black = "000000"
+  var matrix = new Array[Array[String]](height)
+
+  for (y <- 0 until height)
+    matrix(y) = new Array[String](width)
+
+  for ( y <- 0 until height )
+    for ( x <- 0 until width )
+      matrix(y)(x) = black
+
+  def show() : ReactElement = {
+    var list = new mutable.MutableList[ReactElement]
+      for ( y <- 0 until height )
+        for ( x <- 0 until width )
+          list += square(x * (cellSize + margin), y * (cellSize + margin), matrix(y)(x))
+    <.div(list)
+  }
+
+  def set(x: Int, y: Int, color: String) : Unit = {
+    matrix(y)(x) = color
+  }
+
+  private def square(x: Int, y: Int, color: String) : ReactElement = {
+    <.div(^.backgroundColor := "#" + color
+      , ^.width := cellSize + "px"
+      , ^.height := cellSize + "px"
+      , ^.position := "absolute"
+      , ^.left := x + "px"
+      , ^.top := y + "px"
+    )
+  }
+
+}
--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -9,7 +9,9 @@ import scala.scalajs.js.JSApp
 
 object TutorialApp extends JSApp {
   def main(): Unit = {
-    ReactDOM.render(text("Hello world!"), document.body)
+    var display = new LedDisplay(10, 1, 80, 8)
+    display.set(20, 1, "ff0000")
+    ReactDOM.render(display.show(), document.body)
   }
 
   def text(text: String): ReactElement = {

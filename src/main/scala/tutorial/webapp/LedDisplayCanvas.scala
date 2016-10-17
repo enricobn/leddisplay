b9b7b06bd28c7deab2e40477ccc52f5c9835e5c8
Enrico Benedetti <enrico_benedetti@yahoo.com>
2016-10-18 01:27:51 +0200

    LedDisplayCanvas optimization

--- src/main/scala/tutorial/webapp/LedDisplayCanvas.scala
+++ src/main/scala/tutorial/webapp/LedDisplayCanvas.scala
@@ -9,13 +9,21 @@ import scala.collection.immutable.IndexedSeq
 /**
   * Created by enrico on 9/3/16.
   */
+
+object LedDisplayCanvas {
+  val PI2 = 2 * Math.PI
+}
+
 class LedDisplayCanvas(ctx: dom.CanvasRenderingContext2D, cellSize : Int, margin : Int, width : Int, height : Int,
                        onColor: String) {
+  import LedDisplayCanvas._
+
   private val matrix = new Array[Array[Double]](height)
   private val halfCellSize: Int = cellSize / 2
   private val offColorCoeff = 0.2
   private val offColor = deriveColor(onColor, offColorCoeff)
   private val onColorColor = Color(onColor)
+  private var changed = false
 
   for (y <- 0 until height)
     matrix(y) = new Array[Double](width)
@@ -28,6 +36,8 @@ class LedDisplayCanvas(ctx: dom.CanvasRenderingContext2D, cellSize : Int, margin
 //  val offScreenContext = offscreenCanvas.getContext("2d")
 
   def show() {
+    if (!changed) return
+
     ctx.fillStyle = "black"
     ctx.fillRect(0, 0, width * (cellSize + margin) + margin, height * (cellSize + margin) + margin)
 
@@ -35,6 +45,7 @@ class LedDisplayCanvas(ctx: dom.CanvasRenderingContext2D, cellSize : Int, margin
       for ( x <- 0 until width )
         square(ctx, x * (cellSize + margin), y * (cellSize + margin), matrix(y)(x))
 
+    changed = false
 //    var image = offScreenContext.getImageData(0, 0, offscreenCanvas.width, offscreenCanvas.height).asInstanceOf[ImageData]
 //    ctx.putImageData(image, 0, 0)
   }
@@ -43,13 +54,15 @@ class LedDisplayCanvas(ctx: dom.CanvasRenderingContext2D, cellSize : Int, margin
     for ( y <- 0 until height )
       for ( x <- 0 until width )
         matrix(y)(x) = offColorCoeff
+
+    changed = true
   }
 
-  def set(x: Int, y: Int, color: Double) : Unit = {
+  private def set(x: Int, y: Int, color: Double) : Unit = {
     matrix(y)(x) = color
   }
 
-  def print(x: Int, y: Int, c: Char, font: Font) : Unit = {
+  private def print(x: Int, y: Int, c: Char, font: Font) : Unit = {
     val charFont = font.find(_.char.charAt(0) == c)
 
     charFont.foreach(cf => print(x, y, toArray(cf), font))
@@ -58,30 +71,35 @@ class LedDisplayCanvas(ctx: dom.CanvasRenderingContext2D, cellSize : Int, margin
   def print(x: Int, y: Int, s: String, font: Font) : Unit = {
     for (ix <- s.indices)
       print(x + ix * 8, y, s.charAt(ix), font)
+
+    changed = true
   }
 
   def scrollLeft() : Unit = {
     for ( y <- 0 until height )
       for ( x <- 0 until width) {
         val actualValue = matrix(y)(x)
-        val newValue: Double = if (x == width - 1) 0 else matrix(y)(x + 1)
+        val newValue: Double = if (x == width - 1) offColorCoeff else matrix(y)(x + 1)
 
-        matrix(y)(x) =
-          if (newValue == 1) {
-            1
-          } else if (actualValue < offColorCoeff) {
-            offColorCoeff
-          } else {
-            actualValue * 0.8
-          }
-
-        if (x < width - 1) {
-          matrix(y)(x + 1) =
-            if (newValue < offColorCoeff) {
+        if (actualValue != newValue) {
+          matrix(y)(x) =
+            if (newValue == 1) {
+              1
+            } else if (actualValue < offColorCoeff) {
               offColorCoeff
             } else {
-              newValue * 0.8
+              actualValue * 0.8
             }
+
+          if (x < width - 1) {
+            matrix(y)(x + 1) =
+              if (newValue < offColorCoeff) {
+                offColorCoeff
+              } else {
+                newValue * 0.8
+              }
+          }
+          changed = true
         }
       }
   }
@@ -104,7 +122,7 @@ class LedDisplayCanvas(ctx: dom.CanvasRenderingContext2D, cellSize : Int, margin
 
   def fillCircle(ctx: dom.CanvasRenderingContext2D, x: Int, y: Int): Unit = {
     ctx.beginPath()
-    ctx.arc(x + halfCellSize +1, y + halfCellSize +1, halfCellSize, 0, 2 * Math.PI, false)
+    ctx.arc(x + halfCellSize +1, y + halfCellSize +1, halfCellSize, 0, PI2, false)
     ctx.fill()
     ctx.stroke()
   }

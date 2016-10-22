f7dc23a0edb8f122f06194ad6a753396c246c1ac
Enrico Benedetti <enrico_benedetti@yahoo.com>
2016-10-22 02:06:37 +0200

    refactoring

--- src/main/scala/org/leddisplay/OffscreenFont.scala
+++ src/main/scala/org/leddisplay/OffscreenFont.scala
@@ -9,27 +9,27 @@ import org.scalajs.dom.html
 object OffscreenFont {
 
   def read(fontFamily: String, size: Int) : Font = {
-    val offscreenCanvas = dom.document.createElement("Canvas").asInstanceOf[html.Canvas]
-    offscreenCanvas.width = size * 8
-    offscreenCanvas.height = size * 8
-    val offScreenContext = offscreenCanvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
+    val canvas = dom.document.createElement("Canvas").asInstanceOf[html.Canvas]
+    canvas.width = size * 8
+    canvas.height = size * 8
+    val ctx = canvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
 
-    offScreenContext.font = size + "px " + fontFamily
+    ctx.font = size + "px " + fontFamily
 
-    val metrics: FontMetrics = print("HTij", offscreenCanvas, offScreenContext)
+    val metrics: FontMetrics = print("HTij", canvas, ctx)
     val font = new FontImpl(metrics.height)
 
     for (i <- 0 to 255) {
       val c = i.asInstanceOf[Char]
       if (!c.isControl) {
-        offScreenContext.fillStyle = "black"
-        offScreenContext.fillRect(0, 0, offscreenCanvas.width, offscreenCanvas.height)
+        ctx.fillStyle = "black"
+        ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-        offScreenContext.fillStyle = "white"
-        offScreenContext.fillText(c.toString, 0, offscreenCanvas.height / 2)
-        val measure = offScreenContext.measureText(c.toString)
+        ctx.fillStyle = "white"
+        ctx.fillText(c.toString, 0, canvas.height / 2)
+        val measure = ctx.measureText(c.toString)
 
-        val charMetrics: FontMetrics = print(c.toString, offscreenCanvas, offScreenContext)
+        val charMetrics: FontMetrics = print(c.toString, canvas, ctx)
 
         val charWitdh =
           if (charMetrics.width <= 0) {
@@ -41,7 +41,7 @@ object OffscreenFont {
         val charFont = font.getOrCreate(c, () => new CharFontImpl(charWitdh))
         for (y <- metrics.minY to metrics.maxY)
           for (x <- charMetrics.minX to charMetrics.maxX) {
-            val red = charMetrics.data(y * 4 * offscreenCanvas.width + x * 4)
+            val red = charMetrics.data(y * 4 * canvas.width + x * 4)
             val value = getValue(red)
             charFont.set(y - metrics.minY, x - charMetrics.minX, value)
           }
